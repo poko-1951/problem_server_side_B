c90fbc9fca7132d7ff1c5c21d05242b75ad8690c
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-10-09 16:26:17 +0900

    [Add]ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to new_user_post_path(user), notice: "ログイン完了"
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  
+
   resources :users, only: [:new, :create] do
     resources :posts, only: [:new, :create]
   end
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
