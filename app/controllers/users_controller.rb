7eadb3c529e614c645bea79c55d0ee3a726b6aab
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-10-11 23:59:59 +0900

    [Add]フラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to new_user_post_path(@user), notice: "登録完了"
+      redirect_to new_user_post_path(@user), notice: "ユーザー登録完了"
     else
       redirect_to request.referer
     end
